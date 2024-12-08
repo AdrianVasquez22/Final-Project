52272086f9f9e7634f3b0e8b1b0614dbafcd8226
Adrian Vasquez <advasquez2@buffs.wtamu.edu>
2024-12-08 17:43:21 -0600

    Ready For Grading

--- obj/Debug/net8.0/Final Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Final Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9572931cb7156da790fba18711c6e9b13d14815")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
